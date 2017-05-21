f8d8f7aa73adf0c0b58d8f4ae0d40ae6bb758df5
November16 <nxmm1992@gmail.com>
2017-05-21 10:02:37 +0500

    upgrate

--- js/main.js
+++ js/main.js
@@ -1,181 +1,231 @@
-window.onload = function() {
-	var canvas = document.getElementById('podcards'),
-		step = 15,
-		numShapes = 100,
-		direction = 'toMyself',
-		itemsType = 'image',
-		imagesSrc = [
-			'img/images.jpg',
-			'img/images2.jpg',
-			'img/images3.jpg',
-			'img/images4.jpg',
-			'img/images5.jpg',
-			'img/images6.jpg',
-			'img/images7.jpg',
-			'img/images8.jpg',
-			'img/images9.jpg',
-			'img/images10.jpg'
-		];
+function backgroundPoints() {
+	var canvas = document.getElementById('canvas'),
+		ctx = canvas.getContext('2d');
 
-	if(canvas && canvas.getContext) {
-		canvas.width = 10000;
-		canvas.height = 10000;
+	var width = canvas.width = document.body.clientWidth,
+		height = canvas.height = document.body.clientHeight;
+	var points = [],
+		pointsCount = 220;
 
-		var ctx = canvas.getContext('2d'),
-			fl = 400,
-			shapes = [],
-			width = canvas.width = document.documentElement.clientWidth,
-			height = canvas.height = document.documentElement.clientHeight;
+	var userPos = {x: width/2, y: height/2},
+		minDistance = 250;
 
-		for (var i = 0; i < imagesSrc.length; i++) {
-			var img = new Image();
-			img.src = imagesSrc[i];
-			imagesSrc[i] = img;
-		}
+	if(document.documentElement.clientWidth < 768) {
+		pointsCount = 100;
+	}
 
-		for (var i = 0; i < numShapes; i++) {
-			shapes[i] = {
-				x: randomInteger(-1000, 1000),
-				y: randomInteger(-1000, 1000),
-				z: randomInteger(0, 10000),
-				letter: String.fromCharCode(randomInteger(65, 90)),
-				img: imagesSrc[randomInteger(0,9)]
-			};
-		}
+	function getX() {
+		return Math.round(Math.random() * width);
+	}
+	function getY() {
+		return Math.round(Math.random() * height);
+	}
+	function getRandNum(max) {
+		return Math.round(Math.random() * max * 1.5) - max + 0.4;
+	}
+
+	function xSort(a, b) {
+		return a.x - b.x;
+	}
 
 
-		ctx.translate(width / 2, height / 2);
+	for (var i = 0; i < pointsCount; i++) {
+		var point = {
+			x: getX(),
+			y: getY(),
+			speedX: getRandNum(2),
+			speedY: getRandNum(2),
+			size: (Math.round(Math.random() * 5))
+		};
+		points.push(point);
+	}
 
-		function update() {
+	document.body.addEventListener('mousemove', function(event) {
+		userPos.x = event.pageX;
+		userPos.y = event.pageY;
+	});
 
-			ctx.clearRect(-width / 2, -height / 2, width, height);
-			shapes.sort(zsort);
-			for (var i = 0; i < numShapes; i++) {
-	
-				var shape = shapes[i],
-					perspective = fl / (fl + shape.z);
+	update();
+	ctx.globalAlpha = 0.2;
 
-				ctx.save();
-				ctx.scale(perspective, perspective);
-				ctx.translate(shape.x, shape.y);
-				switch(itemsType) {
-					case 'circle': 
-						ctx.beginPath();
-						ctx.arc(-100,-100, 100, 0, Math.PI * 2);
-						ctx.fill();
-						break;
-					case 'square':
-						ctx.fillRect(-100, -100, 200, 200);
-						break;
-					case 'letter':
-						var theLetter = shape.letter;
-						ctx.font = "55pt PT Sans";
-						ctx.fillText(theLetter, -100, -100);
-						break;
-					case 'image':
-						ctx.drawImage(shape.img, -100, -100, 240, 150);
-				}
-				
-				ctx.restore();
+	function update() {
+		ctx.clearRect(0, 0, width, height);
+		var nearlyPoints = [];
 
-				if(direction == 'toMyself') {
-					shape.z -= step;
-					if(shape.z < 0) {
-						shape.z = 10000;
-					}
-				} else {
-					shape.z += step;
-					if(shape.z > 10000) {
-						shape.z = 0;
-					}
-				}
+		for (var i = 0; i < pointsCount; i++) {
+			var point = points[i],
+				d = Math.sqrt((point.x - userPos.x)*(point.x - userPos.x) + (point.y - userPos.y)*(point.y - userPos.y));
+
+
+
+			if(point.x < 0) {
+				point.x = width;
+			}
+			if(point.x > width) {
+				point.x = 0;
+			}
+			if(point.y > height) {
+				point.y = 1;
+			}
+			if(point.y < 0) {
+				point.y = height;
 			}
 
-			requestAnimationFrame(update);
+			ctx.save();
+			ctx.beginPath();
+			ctx.fillStyle = '#000';
+			ctx.arc(point.x, point.y, point.size, 0, Math.PI * 2);
+			ctx.fill();
+			ctx.restore();
+
+			if(d < minDistance) {
+				nearlyPoints.push(point);
+			}
+			point.x += point.speedX;
+			point.y += point.speedY;
 		}
 
-		function init() {
-			menuConfs();
-			update();
+		nearlyPoints.sort(xSort);
+
+		ctx.beginPath();
+		ctx.strokeStyle = 'red';
+		ctx.lineWidth = .20;
+		for (var i = 1; i < nearlyPoints.length; i++) {
+			for (var j = 0; j < nearlyPoints.length; j++) {
+				ctx.moveTo(nearlyPoints[i].x, nearlyPoints[i].y);
+				ctx.lineTo(nearlyPoints[j].x, nearlyPoints[j].y);
+			}
 		}
-		init();
+		ctx.stroke();
+
+		requestAnimationFrame(update);
 	}
+}
+backgroundPoints();
+
+function settingSizes() {
+	// Функция получает массив dom элементов
+	// И задаём им высоту равной их ширине.
+	function setHeightByWidth(elems) {
+		elems.forEach = [].forEach;
+
+		elems.forEach(function(e) {
+			e.style.height = e.clientWidth + 'px';
+		});
+	}
+
+	var stylesCards = document.querySelectorAll('.styles_card');
+	setHeightByWidth(stylesCards);
+}
+settingSizes();
+
+function menuSettings() {
+	var menu = getElementByClass('menuLine');
 	
-	function randomInteger(min, max) {
-	    var rand = min - 0.5 + Math.random() * (max - min + 1)
-	    rand = Math.round(rand);
-	    return rand;
+	setNeedenClassToMenu();
+
+	window.addEventListener('scroll', function(event) {
+		setNeedenClassToMenu();
+	});
+
+	function setNeedenClassToMenu() {
+		if(this.scrollY > menu.clientHeight) {
+			menu.classList.add('menuLine_dark');
+		} else {
+			menu.classList.remove('menuLine_dark');
+		}
+	}
+	if(document.documentElement.clientWidth < 765) {
+		menuConfs();
 	}
 
 	function menuConfs() {
-		var menu = document.querySelector('.menu'),
+		var menu_main = getElementByClass('menu'),
+			menu_main_children = menu_main.querySelectorAll('a'),
 			humburger = document.querySelector('.humburger'),
 			humburgerInner = document.querySelector('.humburgerInner'),
 			isMenuActive = false;
+			menuMaxHeight = menu_main.clientHeight;
 
+		menu_main.style.height = '35px';
+
+		// Humburger Settings
 		humburger.addEventListener('click', function() {
 			if(isMenuActive) {
-				menu.style.left = "-250px";
 				isMenuActive = false;
 				humburgerInner.classList.remove('activeHumburger');
+				menu_main.style.height = '35px';
 			} else {
-				menu.style.left = 0;
 				isMenuActive = true;
 				humburgerInner.className += ' activeHumburger';
+				menu_main.style.height = menuMaxHeight + 'px';
 			}
-		})
+		});
 
-		menu.style.height = document.documentElement.clientHeight + "px";
+		// Menu Elements Settings
+		menu_main_children.forEach = [].forEach;
+		menu_main_children.forEach(function(el) {
+			el.addEventListener('click', function() {
+				console.log('click');
+				menu_main_children.forEach(function(el2) {
+					el2.style.order = 0;
+				})
+				el.style.order = -1;
+			});
+		});
+
+		//////////////////////////////////
+
+		var sectionsOffsets = [
+			{name: 'benefit', value: 0},
+			{name: 'styles', value: 0},
+			{name: 'aboutTeacher', value: 0},
+			{name: 'gallery', value: 0},
+			{name: 'contacts', value: 0},
+		];
+
+		var temp = 0;
+
+		sectionsOffsets.forEach(function(el) {
+			var section = document.getElementById(el.name);
+			temp = el.value = section.offsetTop + document.documentElement.clientHeight/3;
+
+			if(el.name == 'contacts') {
+				el.value = temp + 527;
+			}
+		});
+		console.log(sectionsOffsets);
+
+		window.addEventListener('scroll', function() {
+			temp(0);
+			function temp(i) {
+				if(window.scrollY > sectionsOffsets[sectionsOffsets.length-1].value) {
+					menu_main_children.forEach(function(el2) {
+						el2.style.order = 0;
+					});
+					menu_main_children[menu_main_children.length-1].style.order = -1;
+					return;
+				} else if(window.scrollY > sectionsOffsets[i].value) {
+					// if(++i == sectionsOffsets.length) {
+					// 	menu_main_children.forEach(function(el2) {
+					// 		el2.style.order = 0;
+					// 	});
+					// 	menu_main_children[i-1].style.order = -1;
+					// 	return;
+					// }
+					temp(++i);
+				} else if(window.scrollY > sectionsOffsets[0].value) {
+					menu_main_children.forEach(function(el2) {
+						el2.style.order = 0;
+					});
+					menu_main_children[i-1].style.order = -1;
+				}
+			}
+		});
 	}
+}
+menuSettings();
 
-	function zsort(cardA, cardB) {
-		return cardB.z - cardA.z;
-	}
-
-	var	faster = document.getElementById('faster'),
-		slower = document.getElementById('slower'),
-		standart = document.getElementById('standart'),
-		toMyself = document.getElementById('toMyself'),
-		outMyself = document.getElementById('outMyself'),
-		circle = document.getElementById('circle'),
-		square = document.getElementById('square'),
-		letter = document.getElementById('letter'),
-		image = document.getElementById('image');
-	
-	//Speed
-	faster.addEventListener('click', function() {
-		step += 10;
-	});
-	slower.addEventListener('click', function() {
-		if(!(step < 20)) {
-			step -= 10;
-		} else if(step < 20 && step > 5) {
-			step -= 2;
-		}
-	});
-	standart.addEventListener('click', function() {
-		step = 15;
-	});
-
-	//Items Type
-	circle.addEventListener('click', function() {
-		itemsType = 'circle';
-	});
-	square.addEventListener('click', function() {
-		itemsType = 'square';
-	});
-	letter.addEventListener('click', function() {
-		itemsType = 'letter';
-	});
-	image.addEventListener('click', function() {
-		itemsType = 'image';
-	});
-
-	//Directions
-	toMyself.addEventListener('click', function() {
-		direction = 'toMyself';
-	});
-	outMyself.addEventListener('click', function() {
-		direction = 'outMyself';
-	});
+function getElementByClass(className) {
+return document.querySelector('.' + className) || document.getElementsByClassName(className)[0];
 }

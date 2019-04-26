272965da188541cfea8ad54ec73a22919234fbe1
umid <umid010699@mail.ru>
2019-04-26 18:00:45 +0500

    Release

---
+++ ReactTestTask/precache-manifest.ad89ac3af31972d3359a1a3907f12380.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df5de8ae441bdbd6c283f83436ae1b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c64edc0f0dbcc2c382ec",
+    "url": "/static/css/main.b01b7233.chunk.css"
+  },
+  {
+    "revision": "7add14ba071ed990b486",
+    "url": "/static/js/2.69eab294.chunk.js"
+  },
+  {
+    "revision": "c64edc0f0dbcc2c382ec",
+    "url": "/static/js/main.4e34bb93.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
